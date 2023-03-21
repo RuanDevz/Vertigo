b81ee2b13014f8782704a2dc74e55aa1ba11f159
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-03-21 10:40:49 -0300

    validação completa

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const icon = document.getElementById('icon');
 const passwordInput = document.getElementById('senha');
+const emailInput = document.getElementById('email');
+const entrar = document.getElementById('entrar');
+const msgerro = document.getElementById('msgerro');
+const verificar = /\S+@\S+\.\S+/
+
 icon.addEventListener('click', trocaricon);
 
 function trocaricon() {
@@ -13,3 +18,29 @@ function trocaricon() {
     passwordInput.type = 'password';
   }
 }
+
+entrar.addEventListener('click', function(event) {
+  event.preventDefault(); // Impede que o formulário seja enviado
+
+  // Valida o e-mail
+  if (!emailInput.value || !/\S+@\S+\.\S+/.test(emailInput.value)) {
+    emailInput.style.border = '1px solid #ff0000'
+    msgerroemail.textContent = 'Por favor, informe um endereço de e-mail válido.';
+    return;
+    
+  }
+
+  // Valida a senha
+  if (!passwordInput.value) {
+    passwordInput.style.border = '1px solid #ff0000'
+    msgerrosenha.textContent = 'Por favor, informe a senha.';
+    return;
+  }
+
+  // Aqui você pode continuar com o código para submeter o formulário
+  // ...
+
+  // Limpa a mensagem de erro (caso haja alguma)
+  msgerro.textContent = '';
+});
+
